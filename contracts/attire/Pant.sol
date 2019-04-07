462d459aa34c4e9127779972e322a7a7c198787b
jamestouri <j_touri@yahoo.com>
2019-04-07 14:52:22 -0700

    Add constructor

--- contracts/attire/Pant.sol
+++ contracts/attire/Pant.sol
@@ -1,5 +1,10 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract Pant {
-
+  uint[3] = _volume;
+  constructor(uint256 length, uint256 width, uint256 height) public {
+    _volume[0] = length;
+    _volume[1] = width;
+    _volume[2] = height;
+  }
 }
