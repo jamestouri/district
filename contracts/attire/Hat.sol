288c66ac69130c49597d402741fbf051bc8ca26b
jamestouri <j_touri@yahoo.com>
2019-04-07 18:12:03 -0700

    owner

--- contracts/attire/Hat.sol
+++ contracts/attire/Hat.sol
@@ -1,10 +1,14 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract Hat {
-  uint[3] = _volume;
-  constructor(uint256 length, uint256 width, uint256 height) public {
+  uint[3] private _volume;
+  address private _owner;
+
+  constructor(uint256 length, uint256 width, uint256 height, address owner) public {
     _volume[0] = length;
     _volume[1] = width;
     _volume[2] = height;
+    _owner = owner;
+    
   }
 }
--- contracts/attire/Shirt.sol
+++ contracts/attire/Shirt.sol
@@ -2,9 +2,12 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract Shirt {
   uint[3] = _volume;
-  constructor(uint256 length, uint256 width, uint256 height) public {
+  address private _owner;
+
+  constructor(uint256 length, uint256 width, uint256 height, address owner) public {
     _volume[0] = length;
     _volume[1] = width;
     _volume[2] = height;
+    _owner = owner;
   }
 }
