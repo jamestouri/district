5fb6370fe745e2aff47969e8acfd7d592090a2d3
jamestouri <j_touri@yahoo.com>
2019-04-23 20:48:10 -0700

    Marketplace

---
+++ contracts/marketplace/MarketplaceStorage.sol
@@ -0,0 +1,80 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+contract ERC20Interface {
+  function transferFrom(address from, address to, uint tokens) public returns (bool success);
+}
+
+contract ERC721Interface {
+  function ownerOf(uint256 _tokenId) public view returns (address _owner);
+  function approve(address _to, uint256 _tokenId) public;
+  function getApproved(uint256 _tokenId) public view returns (address);
+  function isApprovedForAll(address _owner, address _operator) public view returns (bool);
+  function safeTransferFrom(address _from, address _to, uint256 _tokenId) public;
+  function supportsInterface(bytes4) public view returns (bool);
+}
+
+
+contract ERC721Verifiable is ERC721Interface {
+  function verifyFingerprint(uint256, bytes) public view returns (bool);
+}
+
+
+contract MarketplaceStorage {
+  ERC20Interface public acceptedToken;
+
+  struct Order {
+    // Order ID
+    bytes32 id;
+    // Owner of the NFT
+    address seller;
+    // NFT registry address
+    address nftAddress;
+    // Price (in wei) for the published item
+    uint256 price;
+    // Time when this sale ends
+    uint256 expiresAt;
+  }
+
+  // From ERC721 registry assetId to Order (to avoid asset collision)
+  mapping (address => mapping(uint256 => Order)) public orderByAssetId;
+
+  uint256 public ownerCutPerMillion;
+  uint256 public publicationFeeInWei;
+
+  address public legacyNFTAddress;
+
+  bytes4 public constant InterfaceId_ValidateFingerprint = bytes4(
+    keccak256("verifyFingerprint(uint256,bytes)")
+  );
+
+  bytes4 public constant ERC721_Interface = bytes4(0x80ac58cd);
+
+  // EVENTS
+  event OrderCreated(
+    bytes32 id,
+    uint256 indexed assetId,
+    address indexed seller,
+    address nftAddress,
+    uint256 priceInWei,
+    uint256 expiresAt
+  );
+  event OrderSuccessful(
+    bytes32 id,
+    uint256 indexed assetId,
+    address indexed seller,
+    address nftAddress,
+    uint256 totalPrice,
+    address indexed buyer
+  );
+  event OrderCancelled(
+    bytes32 id,
+    uint256 indexed assetId,
+    address indexed seller,
+    address nftAddress
+  );
+
+  event ChangedPublicationFee(uint256 publicationFee);
+  event ChangedOwnerCutPerMillion(uint256 ownerCutPerMillion);
+  event ChangeLegacyNFTAddress(address indexed legacyNFTAddress);
+
+}
